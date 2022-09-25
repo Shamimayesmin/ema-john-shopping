0b1bfcb2fb5444fd4e14680fb615c2576fbc4862
shamima <shamimayesmin144@gamil.com>
2022-09-25 22:27:44 +0600

    comment added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Cart.css'
 
+// shopping cart
 const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>order summery</h4>
                 <p>Selected Items : {cart.length}</p>
-                
+
         </div>
     );
 };
